eb401e65de0b4717b8c2f01cff7f157e27840830
Leemet <33695127+markusleemet@users.noreply.github.com>
2020-03-25 14:15:42 +0200

    created add new items list floating action button

--- app/src/main/java/cs/ut/ee/mobilabtestassignment/MainActivity.kt
+++ app/src/main/java/cs/ut/ee/mobilabtestassignment/MainActivity.kt
@@ -2,11 +2,16 @@ package cs.ut.ee.mobilabtestassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun addNewItemsList(v: View){
+
     }
 }
